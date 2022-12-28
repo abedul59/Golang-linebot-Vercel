6297670bceb65c2b9401842dee113911ce6cc06c
abedul59 <70437442+abedul59@users.noreply.github.com>
2022-12-28 18:49:20 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,3 +1,15 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package handler
 
 import (
@@ -5,67 +17,64 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
+var bot *linebot.Client
 
 func Main() {
-	bot, err := linebot.New(
-		os.Getenv("ChannelSecret"),
-		os.Getenv("ChannelAccessToken"),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+	var err error
+	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
+	log.Println("Bot:", bot, " err:", err)
+	http.HandleFunc("/callback", CallbackHandler)
 
-	// Setup HTTP Server for receiving requests from LINE platform
-	http.HandleFunc("/callback", CallbackHandler) 
 	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
 	http.ListenAndServe(addr, nil)
-	
-	
+}
+
 func CallbackHandler(w http.ResponseWriter, r *http.Request) {
-		events, err := bot.ParseRequest(r)
-	
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
-			return
+	events, err := bot.ParseRequest(r)
+
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
 		}
-	
-		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				// Handle only on text message
-				case *linebot.TextMessage:
-					// GetMessageQuota: Get how many remain free tier push message quota you still have this month. (maximum 500)
-					quota, err := bot.GetMessageQuota().Do()
-					if err != nil {
-						log.Println("Quota err:", err)
-					}
-					// message.ID: Msg unique ID
-					// message.Text: Msg text
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("msg ID:"+message.ID+":"+"Get:"+message.Text+" , \n OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
-						log.Print(err)
-					}
-	
-				// Handle only on Sticker message
-				case *linebot.StickerMessage:
-					var kw string
-					for _, k := range message.Keywords {
-						kw = kw + "," + k
-					}
-	
-					outStickerResult := fmt.Sprintf("收到貼圖訊息: %s, pkg: %s kw: %s  text: %s", message.StickerID, message.PackageID, kw, message.Text)
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(outStickerResult)).Do(); err != nil {
-						log.Print(err)
-					}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			// Handle only on text message
+			case *linebot.TextMessage:
+				// GetMessageQuota: Get how many remain free tier push message quota you still have this month. (maximum 500)
+				quota, err := bot.GetMessageQuota().Do()
+				if err != nil {
+					log.Println("Quota err:", err)
+				}
+				// message.ID: Msg unique ID
+				// message.Text: Msg text
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("msg ID:"+message.ID+":"+"Get:"+message.Text+" , \n OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
+					log.Print(err)
+				}
+
+			// Handle only on Sticker message
+			case *linebot.StickerMessage:
+				var kw string
+				for _, k := range message.Keywords {
+					kw = kw + "," + k
+				}
+
+				outStickerResult := fmt.Sprintf("收到貼圖訊息: %s, pkg: %s kw: %s  text: %s", message.StickerID, message.PackageID, kw, message.Text)
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(outStickerResult)).Do(); err != nil {
+					log.Print(err)
 				}
 			}
 		}
 	}
+}
