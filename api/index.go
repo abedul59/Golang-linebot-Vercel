11da3b7bcc9295d02ae62b1f6595f475f4319916
abedul59 <70437442+abedul59@users.noreply.github.com>
2022-12-28 18:28:34 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -29,8 +29,8 @@ func Main(w http.ResponseWriter, r *http.Request) {
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	log.Println("Bot:", bot, " err:", err)
 	http.HandleFunc("/callback", CallbackHandler)
-
-	port := 80 #os.Getenv("PORT")
+        http.HandleFunc("/", CallbackHandler)
+	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
 	http.ListenAndServe(addr, nil)
 }
